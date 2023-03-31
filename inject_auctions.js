dbfa4011eda4e8a1053198a2bf42b256517596b6
Ryan Ewen <ryan.ewen@gmail.com>
2023-03-30 23:25:41 -0400

    Remove JS injection from auctions page

--- inject_auctions.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    function forward(action, args) {
-        window.postMessage({ from: 'AUCTIONS', message: { action, args } })
-    }
-
-    // push some basic user info and watched listing names
-    forward('AUCTIONS_PAGE_LOADED')
-}())
